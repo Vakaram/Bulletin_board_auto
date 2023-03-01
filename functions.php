59d9e8afdbcebe5acf6978265577aef42ef61798
Vakaram <Vakaramm@gmail.com>
2023-03-01 23:25:07 +0500

    ну чё пока застрял с функцие коннекта блин

--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 <?php
 
-    function db ($host,$user,$pass,$db_name) {
-        $db = mysql_connect($host,$user,$pass); #тут во первых поменять на новую функцию, во вторых если подключение дуачно тоон запишет нам в переменную данные
+    function db ($host,$user,$pass,$db_name){
+        $db = mysqli_connect($host,$user,$pass);#тут во первых поменять на новую функцию, во вторых если подключение дуачно тоон запишет нам в переменную данные
         if(!$db){
-            exit(mysql_error());#если не подключился то выходим просто из функции
+            exit(mysqli_error());#если не подключился то выходим просто из функции
         }
-        if (!mysql_select_db($db_name,$db)){
-            exit(mysql_error());
+        if (!mysqli_select_db($db_name,$db)){
+            exit(mysqli_error());
         }
-        mysql_query("SET NAMES UTF8"); #функция даёт возмодность отправить запрос в Бд. отправим запрос для определения кодировки
+        mysqli_query("SET NAMES UTF8"); #функция даёт возмодность отправить запрос в Бд. отправим запрос для определения кодировки
     }
 
 
